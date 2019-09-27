43896a1cecbc1bd5749a754387b4a10d88ee2855
Alfredo Arias <alfredo@xiobit.com>
2019-09-27 10:10:53 -0400

    firebase auth (login y registro en forebase)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+    production: true,
+    firebase: {
+        apiKey: 'AIzaSyAZqLtnUhnYyW5bck627w-e0LGTyavWeC8',
+        authDomain: 'geekyplant.firebaseapp.com',
+        databaseURL: 'https://geekyplant.firebaseio.com',
+        projectId: 'geekyplant',
+        storageBucket: 'geekyplant.appspot.com',
+        messagingSenderId: '219280136249',
+        appId: '1:219280136249:web:2aafe8f80e32ede9'
+    }
 };
